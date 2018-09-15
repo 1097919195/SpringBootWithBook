b5579ecfca2ef2d61e82970187bbe2bde73d69b8
zjl7577 <1097919195@qq.com>
2018-09-15 17:11:16 +0800

    format perfect

--- src/main/java/demo/user/userController.java
+++ src/main/java/demo/user/userController.java
@@ -15,7 +15,6 @@ import demo.util.ResponseData;
 import demo.util.redis.JedisUtil;
 
 
-
 /**
  * @功能:
  * @项目名:itcast-springboot2
@@ -25,39 +24,42 @@ import demo.util.redis.JedisUtil;
 @RestController
 @RequestMapping("/mngUser")
 public class userController {
-	@Autowired
-	private userService userservice;
-	 @Autowired
-	 private  JedisUtil jedisUtil;
-	//用户登陆
-	@RequestMapping("/addLogin")
-	public ResponseData<userPojo> addLogin(userPojo user, HttpServletRequest request,
-			HttpServletResponse response) {
-		return userservice.login(user);
-	}
-	//新增用户
-	@RequestMapping("/insertOne")
-	public ResponseData<userPojo> insertOne(userPojo user, HttpServletRequest request,
-			HttpServletResponse response) {
-		return userservice.insertUser(user);
-	}
-	//修改密码
-	@RequestMapping("/update")
-	public ResponseData<userPojo> update(userPojo user, HttpServletRequest request,
-			HttpServletResponse response) {
-		return userservice.change(user);
-	}
-	
-	//退出登录
-	@RequestMapping("/quitLogin")
-	public ResponseData<String> quitLogin(String userId, HttpServletRequest request,HttpServletResponse response) {
-		 ResponseData<String> data=new ResponseData<String>();
-		if(jedisUtil.getByKey("userToken"+userId)!=null){
-			 jedisUtil.delByKey("userToken"+userId);//清除token
-			 data.setTokenState(0);
-		}else{
-			 data.setTokenState(1);
-		}
-		return data;
-	}
+    @Autowired
+    private userService userservice;
+    @Autowired
+    private JedisUtil jedisUtil;
+
+    //用户登陆
+    @RequestMapping("/addLogin")
+    public ResponseData<userPojo> addLogin(userPojo user, HttpServletRequest request,
+                                           HttpServletResponse response) {
+        return userservice.login(user);
+    }
+
+    //新增用户
+    @RequestMapping("/insertOne")
+    public ResponseData<userPojo> insertOne(userPojo user, HttpServletRequest request,
+                                            HttpServletResponse response) {
+        return userservice.insertUser(user);
+    }
+
+    //修改密码
+    @RequestMapping("/update")
+    public ResponseData<userPojo> update(userPojo user, HttpServletRequest request,
+                                         HttpServletResponse response) {
+        return userservice.change(user);
+    }
+
+    //退出登录
+    @RequestMapping("/quitLogin")
+    public ResponseData<String> quitLogin(String userId, HttpServletRequest request, HttpServletResponse response) {
+        ResponseData<String> data = new ResponseData<String>();
+        if (jedisUtil.getByKey("userToken" + userId) != null) {
+            jedisUtil.delByKey("userToken" + userId);//清除token
+            data.setTokenState(0);
+        } else {
+            data.setTokenState(1);
+        }
+        return data;
+    }
 }
